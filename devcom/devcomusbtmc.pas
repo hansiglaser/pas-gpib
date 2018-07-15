740bd75a0ca32ade3114ed0e88b24be46a1a1e4b
Johann Glaser <Johann.Glaser@gmx.at>
2018-07-15 10:44:16 +0200

    devcom/devcomusbtmc.pas  - improved waiting for result message by using the Status Byte  - added (optional) error handling

--- devcom/devcomusbtmc.pas
+++ devcom/devcomusbtmc.pas
@@ -14,6 +14,8 @@ Uses
   DevCom, UsbTmc;
 
 Type
+  TErrorHandler = Procedure;    // raises an exception if an unrecoverable error has happend
+
   { TUSBTMCCommunicator }
 
   (**
@@ -27,6 +29,8 @@ Type
   private
     FDevice       : TUSBTMCUSB488;
     FTransferSize : Integer;
+    FErrorHandler       : TErrorHandler;
+    FEnableErrorHandler : Boolean;
   public
     Constructor Create(ADevice : TUSBTMCUSB488);
     { interface IDeviceCommunicator }
@@ -35,8 +39,12 @@ Type
     Function  Query(St: String): String;
     Procedure SetTimeout(ATimeout:LongInt);   // in us
     { USBTMC functions }
+    Function WaitMessage(MinWaitMs : LongInt) : Byte;
+    Procedure CheckErrors(Status : Byte);
+    Procedure CheckErrors;
     // (maximum) number of bytes used for DEV_DEP_MSG_IN
     property TransferSize : Integer read FTransferSize write FTransferSize;
+    property ErrorHandler : TErrorHandler read FErrorHandler write FErrorHandler;
   End;
 
 Implementation
@@ -48,6 +56,7 @@ Begin
   FDevice := ADevice;
   SetTimeout(500000{us});  // default to 500ms
   FTransferSize := 2048;
+  FEnableErrorHandler := True;
 End;
 
 Procedure TUSBTMCCommunicator.Send(St:String);
@@ -56,19 +65,94 @@ Begin
 End;
 
 Function TUSBTMCCommunicator.Receive : String;
+Var Status : Byte;
 Begin
+  // wait until a message is available (with FDevice.Timeout, in steps of 10ms)
+  Status := WaitMessage(10);
+  if (Status and IEEE488_StatusByte_MessageAvailable) = 0 then
+    Begin
+      // no message available, is there an error?
+      CheckErrors(Status);     // will call FErrorHandler, which might raise an exception
+      raise Exception.Create('TUSBTMCCommunicator.Receive: Timeout, no message available');
+    End;
+
+  // get the message
   Result := FDevice.Recv(FTransferSize);
+  // if there was a further error, this has to be checked by the caller, e.g., Query
 End;
 
 Function TUSBTMCCommunicator.Query(St:String) : String;
+Var Status  : Byte;
+    Message : String;
 Begin
+  // first check if there was an error with a previous command
+  Status := FDevice.ReadStatusByte;
+  CheckErrors(Status);
+  // check if an old message is still waiting
+  if (Status and IEEE488_StatusByte_MessageAvailable) <> 0 then
+    Begin
+      Message := Trim(FDevice.Recv(FTransferSize));
+      raise Exception.Create('Error: Before sending '''+St+''', a message from the device was available: '''+Message+'''');
+    End;
+
+  // send the command
   Send(St);
+  // wait for reply
   Result := Trim(Receive);
+
+  // finally, check if there was an error with the current command
+  CheckErrors;   // has its own ReadStatusByte
 End;
 
 Procedure TUSBTMCCommunicator.SetTimeout(ATimeout:LongInt);
 Begin
   FDevice.Timeout := (ATimeout + 500) div 1000;   // round to nearest value in ms
+End;
+
+(**
+ * Wait until a message is available
+ *
+ * Every MinWaitMs it is checked, whether a message is available.
+ * The total timeout is FDevice.Timeout.
+ *)
+Function TUSBTMCCommunicator.WaitMessage(MinWaitMs : LongInt) : Byte;
+Var Timeout : QWord;
+    Status  : Byte;
+Begin
+  TimeOut := GetTickCount64 + FDevice.Timeout;
+  repeat
+    Status := FDevice.ReadStatusByte;
+    if (Status and IEEE488_StatusByte_MessageAvailable) <> 0 then
+      Exit(Status);
+    Sleep(MinWaitMs);
+  Until GetTickCount64 > Timeout;
+  Result := Status;
+End;
+
+(**
+ * Check if the StatusByte signals an error, then execute the error handler
+ *)
+Procedure TUSBTMCCommunicator.CheckErrors(Status:Byte);
+Var OldEnableErrorHandler : Boolean;
+Begin
+  if FEnableErrorHandler and assigned(FErrorHandler) and ((Status and IEEE488_StatusByte_ErrorQueue) <> 0) then
+    Begin
+      try
+        OldEnableErrorHandler := FEnableErrorHandler;
+        FEnableErrorHandler := False;   // no error checking while error handling
+        FErrorHandler;
+      finally
+        FEnableErrorHandler := OldEnableErrorHandler;
+      End;
+    End;
+End;
+
+Procedure TUSBTMCCommunicator.CheckErrors;
+Var Status : Byte;
+Begin
+  if not (FEnableErrorHandler and assigned(FErrorHandler)) then Exit;
+  Status := FDevice.ReadStatusByte;
+  CheckErrors(Status);
 End;
 
 End.
